c0f414b5901c02168018e9ef10bd39355cd243f2
ShatilKhan <shatilshahriar009@gmail.com>
2023-07-01 18:08:19 +0600

    Add connection tp database
    At the very begining of app.js use the variables to connect to mongodb & hedera

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { useState } from 'react';
 import QrScanner from 'react-qr-scanner';
 import dotenv from 'dotenv/config';
+import { Client } from '@hashgraph/sdk';
+import mongodb from 'mongodb';
+
+
+// Replace with your Hedera network details
+const operatorAccount = process.env.REACT_APP_OPERATOR_ACCOUNT_ID;
+const operatorPrivateKey = process.env.REACT_APP_OPERATOR_PRIVATE_KEY;
+const hederaClient = Client.forTestnet();
+hederaClient.setOperator(operatorAccount, operatorPrivateKey);
+const client = new mongodb.MongoClient(process.env.REACT_APP_MONGODB_CONNECTION_URL);
+
+
 
 function App() {
   const [qrData, setQrData] = useState(null);
 
-  const handleScan = (data) => {
+  const handleScan = async (data) => {
     if (data) {
       setQrData(data);
+      const fabricId = data; // Assuming the QR code directly contains the fabric ID
+  
+      // Connect to MongoDB
+      const client = new mongodb.MongoClient('<YOUR_MONGODB_CONNECTION_URL>');
+      await client.connect();
+  
+      // Retrieve fabric information
+      const db = client.db('<YOUR_DATABASE_NAME>');
+      const collection = db.collection('<YOUR_COLLECTION_NAME>');
+      const fabric = await collection.findOne({ fabricId });
+  
+      // Close MongoDB connection
+      await client.close();
+  
+      if (fabric) {
+        // Display fabric information
+        console.log(fabric);
+      }
     }
   };
+  
 
   const handleError = (error) => {
     console.error(error);
